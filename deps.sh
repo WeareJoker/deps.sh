6e62dc77a4ec829fcb0ac5de4a9752391f7f399e
DMJ <zenike@naver.com>
2016-12-14 00:55:29 +0000

    Removed libtins & dot11decrypt and Added wdecrypt and created another shell which set the raspberry AP mode.

--- deps.sh
+++ deps.sh
@@ -1,4 +1,3 @@
-
 #update and upgrade
 sudo apt-get update
 sudo apt-get upgrade
@@ -9,21 +8,27 @@ sudo pip3 install scapy-python3
 #dependency for libtins and aircrack
 sudo apt-get install libpcap-dev libssl-dev cmake aircrack-ng -y
 
-#libtins
-sudo git clone https://github.com/mfontanini/libtins.git
-mkdir build
-cd build
-sudo cmake ../
-sudo make
-sudo make install 
+#libtins - not used now
+#sudo git clone https://github.com/mfontanini/libtins.git
+#cd libtins
+#mkdir build
+#cd build
+#sudo cmake ../
+#sudo make
+#sudo make install 
 
-#dot11decrypt
-sudo git clone https://github.com/mfontanini/dot11decrypt
-mkdir build
-cd build
-sudo cmake ../
-sudo make
+#dot11decrypt - not used now
+#sudo git clone https://github.com/mfontanini/dot11decrypt
+#cd dot11decrypt
+#mkdir build
+#cd build
+#sudo cmake ../
+#sudo make
 
+#wdecrypt - new update
+git clone https://github.com/snoopspy/wdecrypt
+cd wdecrypt
+sudo make
 
 # screen for executing different programs in one terminal.
 sudo apt-get install screen -y
