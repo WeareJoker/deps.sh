402607cd7fbcc83e7ad16c1802274eba40fc7c45
DMJ <zenike@naver.com>
2016-12-14 01:10:09 +0000

    Added git clone ADHOC_PRESETTING and deleted wdecrypt

--- deps.sh
+++ deps.sh
@@ -2,37 +2,51 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+# dependency for libtins and aircrack
+sudo apt-get install libpcap-dev libssl-dev cmake aircrack-ng -y
+
+
 #scapy for python3
 sudo pip3 install scapy-python3
 
-#dependency for libtins and aircrack
-sudo apt-get install libpcap-dev libssl-dev cmake aircrack-ng -y
-
-#libtins - not used now
-#sudo git clone https://github.com/mfontanini/libtins.git
-#cd libtins
-#mkdir build
-#cd build
-#sudo cmake ../
-#sudo make
-#sudo make install 
-
-#dot11decrypt - not used now
-#sudo git clone https://github.com/mfontanini/dot11decrypt
-#cd dot11decrypt
-#mkdir build
-#cd build
-#sudo cmake ../
-#sudo make
-
-#wdecrypt - new update
-git clone https://github.com/snoopspy/wdecrypt
-cd wdecrypt
-sudo make
-
-# screen for executing different programs in one terminal.
-sudo apt-get install screen -y
 
 # database library for python3
 sudo apt-get install sqlite3
 sudo pip3 install sqlalchemy
+
+
+# The main code - offers web interfaces and sniffing tools
+# it needs python flask -- ADD ANOTHER SHELL PLEASE
+git clone https://github.com/WeareJoker/ADHOC_PRESETTING
+cd ADHOC_PRESETTING/wdecrypt
+sudo make
+cd ../../
+
+# screen for executing different programs in one terminal (download if you need)
+# sudo apt-get install screen -y
+
+
+#wdecrypt
+# If you already downloaded(cloned) "ADHOC_PRESETTING"
+# You don't need these because "ADHOC_PRESETTING" includes these.
+# git clone https://github.com/snoopspy/wdecrypt
+# cd wdecrypt
+# sudo make
+
+
+# libtins - not used now
+# sudo git clone https://github.com/mfontanini/libtins.git
+# cd libtins
+# mkdir build
+# cd build
+# sudo cmake ../
+# sudo make
+# sudo make install 
+
+# dot11decrypt - not used now
+# sudo git clone https://github.com/mfontanini/dot11decrypt
+# cd dot11decrypt
+# mkdir build
+# cd build
+# sudo cmake ../
+# sudo make
